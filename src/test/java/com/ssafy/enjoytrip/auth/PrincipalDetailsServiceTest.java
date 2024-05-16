a58c3fb90534b7f223628dc4e83649b445c39060
songdoeon <ehdjs9583@naver.com>
2024-05-16 16:31:43 +0900

    loginTestcode-done

--- src/test/java/com/ssafy/enjoytrip/auth/PrincipalDetailsServiceTest.java
+++ src/test/java/com/ssafy/enjoytrip/auth/PrincipalDetailsServiceTest.java
@@ -4,22 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.enjoytrip.auth.dto.LoginRequestDto;
 import com.ssafy.enjoytrip.member.domain.Member;
 import com.ssafy.enjoytrip.member.repository.MemberRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PrincipalDetailsServiceTest {
     @Autowired
     MockMvc mvc;
@@ -55,6 +56,7 @@ class PrincipalDetailsServiceTest {
     }
     @Test
     @DisplayName("로그인 성공")
+    @Order(1)
     public void login() throws Exception{
         memberRepository.save(member);
         mvc.perform(post("/login")
@@ -65,21 +67,24 @@ class PrincipalDetailsServiceTest {
             .andExpect(status().isOk());
     }
     @Test
+    @Order(2)
     @DisplayName("잘못된 비밀번호로 로그인")
     public void NotMatchedPassword() throws Exception{
-        memberRepository.save(member);
         mvc.perform(post("/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(notMatchedPasswordRequestDto))
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isUnauthorized());
     }
     @Test
+    @Order(3)
     @DisplayName("로그아웃")
     public void logout() throws Exception{
         mvc.perform(post("/logout"))
                 .andDo(print())
+                .andExpect(redirectedUrl("/"))
                 .andExpect(status().is3xxRedirection());
+
     }
 }
