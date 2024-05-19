f3aa2af6e0df016bdae29e9e8e33fa166f56eb3f
Songdoeon <ehdjs9583@naver.com>
2024-05-19 19:32:00 +0900

    memberRepository findByName 추가

--- src/main/java/com/ssafy/enjoytrip/member/repository/MemberRepository.java
+++ src/main/java/com/ssafy/enjoytrip/member/repository/MemberRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(String id);
+    Optional<Member> findByName(String name);
     Boolean existsById(String id);
 }
